3c24a11f1ff06ee653be9086b7b555359edc55af
codyshiveley2-ux <codyshiveley2@gmail.com>
2026-02-28 07:37:43 -0500

    Current Weekly Changes
    Lists Assignments

--- food.py
+++ food.py
@@ -14,3 +14,20 @@
 #Your entered foods are:
 #pizza, beef jerkey, rice triangles, steamed chinese bun, fried chicken 
 #You entered a total of 62 characters
+
+
+foods = []
+
+
+for i in range(5):
+    food = input("Enter a food: ")
+    foods.append(food)
+
+
+print("\nYour entered foods are:")
+print(", ".join(foods))
+
+
+total_chars = sum(len(item) for item in foods)
+
+print(f"You entered a total of {total_chars} characters")
--- meanmedian.py
+++ meanmedian.py
@@ -4,3 +4,40 @@
 #This is a guided practice. You can follow the video or your instructor will go
 #over this in class
 # video url: https://youtu.be/VGrRdUunjXg
+
+
+numbers = []
+
+for i in range(5):
+    num = float(input("Enter a number: "))
+    numbers.append(num)
+
+
+print("\nNumbers as entered:")
+print(numbers)
+
+
+ascending = sorted(numbers)
+print("Small to large:")
+print(ascending)
+
+
+descending = sorted(numbers, reverse=True)
+print("Large to small:")
+print(descending)
+
+
+mean = sum(numbers) / len(numbers)
+
+1
+ascending_sorted = sorted(numbers)
+mid = len(ascending_sorted) // 2
+
+if len(ascending_sorted) % 2 == 1:
+    median = ascending_sorted[mid]
+else:
+    median = (ascending_sorted[mid - 1] + ascending_sorted[mid]) / 2
+
+print(f"\nMean: {mean}")
+print(f"Median: {median}")
+
